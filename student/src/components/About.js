a61d2b9fb8b777cb8df40e165e65e795ba15f6a3
Inshafa-Kaleel <154611829+Inshafa-Kaleel@users.noreply.github.com>
2024-12-02 03:43:29 +0000

    create setting page

---
+++ student/src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const About = () => (
+  <div className="about-container">
+    <h1>About Us</h1>
+    <p>Welcome to our React project! Learn and grow with this simple app.</p>
+  </div>
+);
+
+export default About;
+
