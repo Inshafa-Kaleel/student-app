d9e42f504705984e643d936ffb1277f320e53d66
Inshafa-Kaleel <154611829+Inshafa-Kaleel@users.noreply.github.com>
2024-12-02 03:45:06 +0000

    correct the app.js file path

---
+++ student/src/components/Settings.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+const Settings = () => (
+  <div className="settings-container">
+    <h1>Settings</h1>
+    <ul>
+      <li>Notification Preferences</li>
+      <li>Change Password</li>
+      <li>Privacy Settings</li>
+    </ul>
+  </div>
+);
+
+export default Settings;
